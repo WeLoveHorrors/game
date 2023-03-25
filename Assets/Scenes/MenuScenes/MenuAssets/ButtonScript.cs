75b9eb0637ee7d6b8c9d3d05a1d27f519f3914ca
Fetu56 <86706321+Fetu56@users.noreply.github.com>
2023-03-25 02:56:06 +0200

    Standart chill 113 changes commit from Maxim
    XD

---
+++ Assets/Scenes/MenuScenes/MenuAssets/ButtonScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+    public TMPro.TextMeshPro theText;
+    public Color color;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
