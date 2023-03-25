16316136c2020736df7361bec5452bbeb3de91c8
Fetu56 <86706321+Fetu56@users.noreply.github.com>
2023-03-25 04:59:38 +0200

    Average commit enjoyer, added menu+functional buttons+volume setting

---
+++ Assets/Scenes/MenuScenes/MenuAssets/Scripts/ButtonScript.cs
@@ -0,0 +1,42 @@
+using Assets.Scripts;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour
+{
+    public TMPro.TextMeshProUGUI Text;
+    public Color ColorStandart;
+    public Color ColorOnHover;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Enter");
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("Exit");
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Enter");
+        Text.colorGradient = new TMPro.VertexGradient(Color.black, Text.colorGradient.bottomLeft, Text.colorGradient.topRight, Text.colorGradient.bottomRight);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("Exit");
+        Text.colorGradient = new TMPro.VertexGradient(ColorStandart, Text.colorGradient.bottomLeft, Text.colorGradient.topRight, Text.colorGradient.bottomRight);
+    }
+    
+    public void StartGame()
+    {
+        SceneManager.LoadScene((int)Scenes.Game);
+    }
+    public void ExitGame()
+    {
+        Debug.Log("Exiting game...");
+        Application.Quit();
+    }
+}
