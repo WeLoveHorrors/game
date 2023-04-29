19e6caa3c8d3c677d3fd765d5f85670f2987b0fe
Crustbrns <georgejoof@gmail.com>
2023-04-29 17:24:04 +0300

    Added proper bullet holes & falling cartridges

--- Assets/Scenes/GunScene/GunSystem.cs
+++ Assets/Scenes/GunScene/GunSystem.cs
@@ -20,6 +20,7 @@ public class GunSystem : MonoBehaviour
     public GameObject bulletHoleGraphic;
     public ParticleSystem muzzleFlash;
     public ParticleSystem ribbonSmoke;
+    public ParticleSystem cartridge;
 
     public bool isRibbonEnabled;
     public float RibbonTimeAlive = 2f;
@@ -55,35 +56,38 @@ public class GunSystem : MonoBehaviour
         else
         {
             GetComponent<Animator>().SetBool("isShooting", false);
+            HandleRibbon();
+            bulletsShot = 0;
         }
 
-        if(isRibbonEnabled && !shooting)
-        {
-            bulletsShot = 0;
-            RibbonTimeAlive -= Time.deltaTime;
-            if(RibbonTimeAlive > 0)
-            {
-                ribbonSmoke.Play();
-            }
-            else
-            {
-                ribbonSmoke.Stop();
-                isRibbonEnabled = false;
-            }
-            //  = new Color(255,255,255,0);
-        }
+        // if(isRibbonEnabled && !shooting)
+        // {
+        //     bulletsShot = 0;
+        //     RibbonTimeAlive -= Time.deltaTime;
+        //     if(RibbonTimeAlive > 0)
+        //     {
+        //         ribbonSmoke.Play();
+        //     }
+        //     else
+        //     {
+        //         ribbonSmoke.Stop();
+        //         isRibbonEnabled = false;
+        //     }
+        //     //  = new Color(255,255,255,0);
+        // }
     }
 
     private void Shoot()
     {
-        if(readyToShoot)
+        if(readyToShoot && bulletsLeft > 0)
         {
             bulletsLeft--;
             bulletsShot++;
             readyToShoot = false;
+            cartridge.Play();
 
-            float x = bulletsShot == 0 ? 0 : Random.Range(-spread, spread) + bulletsShot * spread * Random.Range(-0.4f, 0.4f);
-            float y = bulletsShot == 0 ? 0 : Random.Range(-spread, spread) + bulletsShot * spread * Random.Range(0.02f, 0.05f);
+            float x = bulletsShot == 0 ? 0 : Random.Range(-spread, spread) + bulletsShot * spread * Random.Range(-0.2f, 0.2f);
+            float y = bulletsShot == 0 ? 0 : Random.Range(-spread, spread) + bulletsShot * spread * Random.Range(0.02f, 0.06f);
 
             Vector3 direction = fpsCam.transform.forward + new Vector3(x, y, 0);
 
@@ -96,12 +100,40 @@ public class GunSystem : MonoBehaviour
                     // rayHit.collider.GetComponent
                 }
             }
-
-            Instantiate(bulletHoleGraphic, rayHit.point, Quaternion.Euler(0, 90, 0));
-            muzzleFlash.Play();
             
+            HandleAllBulletsLeftRibbon();
+
+            Instantiate(bulletHoleGraphic, rayHit.point + (rayHit.normal * .01f), Quaternion.LookRotation(rayHit.normal));
+            muzzleFlash.Play();
+
             Invoke("ResetShot", timeBetweenShooting); 
         }
+    }
+    private void HandleRibbon()
+    {
+        if(bulletsShot != 30 && bulletsShot > 20 && !shooting)
+        {
+            Instantiate(bulletHoleGraphic, rayHit.point, Quaternion.Euler(0, 90, 0));
+            isRibbonEnabled = true;
+            RibbonTimeAlive = 2f;
+            ribbonSmoke.Play();
+            Invoke("StopRibbon", 2f);
+        }
+    }
+    private void HandleAllBulletsLeftRibbon()
+    {
+        if(bulletsLeft <= 0 && bulletsShot == 30)
+        {
+            isRibbonEnabled = true;
+            RibbonTimeAlive = 3f;
+            ribbonSmoke.Play();
+            Invoke("StopRibbon", 3f);
+        }
+    }
+
+    private void StopRibbon()
+    {
+        ribbonSmoke.Stop();
     }
 
     private void ResetShot()
