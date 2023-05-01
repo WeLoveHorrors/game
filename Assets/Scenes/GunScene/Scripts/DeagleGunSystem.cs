f32840875538e172670057bf69777d63f3f123b9
Crustbrns <georgejoof@gmail.com>
2023-05-01 10:26:48 +0300

    Added recoils for pistol

--- Assets/Scenes/GunScene/Scripts/DeagleGunSystem.cs
+++ Assets/Scenes/GunScene/Scripts/DeagleGunSystem.cs
@@ -7,10 +7,12 @@ public class DeagleGunSystem : MonoBehaviour
     public Animator m_animator;
     bool shooting, readyToShoot, reloading;
     float bulletsLeft;
+    public float timeBetweenShooting;
 
     void Awake()
     {
         bulletsLeft = 100;
+        Invoke("AllowShoot", 0.1f);
         // Invoke("AllowShoot", 0.25f);
     }
 
@@ -24,11 +26,24 @@ public class DeagleGunSystem : MonoBehaviour
     {
         shooting = Input.GetKey(KeyCode.Mouse0);
         
-        if(shooting) 
+        if(readyToShoot && shooting) 
         {
             bulletsLeft = 100;
-        }
-        
-        m_animator.SetBool("Shooting", shooting);
+            m_animator.SetBool("Shooting", shooting);
+            readyToShoot = false;
+            GetComponentInChildren<DeagleWeaponRecoil>().Fire();
+            
+            Invoke("ResetShot", timeBetweenShooting); 
+        } 
+    }
+    
+    private void ResetShot()
+    {
+        readyToShoot = true;
+    }
+
+    private void AllowShoot()
+    {
+        readyToShoot = true;
     }
 }
---
+++ Assets/Scenes/GunScene/Scripts/DeagleWeaponRecoil.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeagleWeaponRecoil : MonoBehaviour
+{
+    [Header("Reference Points")]
+	public Transform recoilPosition;
+	public Transform rotationPoint;
+	Vector3 Rot;
+
+	Vector3 rotationalRecoil;
+	Vector3 positionalRecoil;
+    
+	public Vector3 RecoilRotation = new Vector3(10, 5, 7);
+	public Vector3 RecoilKickBack = new Vector3(0.015f, 0f, -0.2f);
+    
+    [Space(10)]
+	public float positionalReturnSpeed = 20f;
+	public float rotationalReturnSpeed = 44.5f;
+
+	[Header("Speed Settings")]
+	public float positionalRecoilSpeed = 30f;
+	public float rotationalRecoilSpeed = 70f;
+
+    private void Awake()
+    {
+	}
+
+    // Update is called once per frame
+    void Update()
+	{
+		// if (Input.GetKey(KeyCode.Mouse0))
+		// {
+		// 	Fire();
+		// }
+	}
+
+	public void Fire()
+	{
+        float movingRight = Input.GetAxisRaw("Horizontal");
+        positionalRecoil += new Vector3(0.0055f, 0.17f, 0f);
+		rotationalRecoil += new Vector3(RecoilRotation.x * (movingRight >= 0 ? 1 : 0.35f), 2f, Random.Range(-RecoilRotation.z, RecoilRotation.z));
+		rotationalRecoil += new Vector3(Random.Range(-RecoilKickBack.x, RecoilKickBack.x), Random.Range(-RecoilKickBack.y, RecoilKickBack.y), RecoilKickBack.z);
+        
+		recoilPosition.localPosition = Vector3.Slerp(recoilPosition.localPosition, positionalRecoil, positionalRecoilSpeed * Time.deltaTime);
+		Rot = Vector3.Slerp(Rot, rotationalRecoil, rotationalRecoilSpeed * Time.deltaTime);
+		rotationPoint.localRotation = Quaternion.Euler(Rot);
+    }
+
+	private void FixedUpdate()
+	{
+		rotationalRecoil = Vector3.Slerp(rotationalRecoil, Vector3.zero, rotationalReturnSpeed * Time.deltaTime);
+		positionalRecoil = Vector3.Slerp(positionalRecoil, Vector3.zero, positionalReturnSpeed * Time.deltaTime * 0.6f);
+    }
+}
