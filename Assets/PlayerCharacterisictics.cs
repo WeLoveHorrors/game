eb83e0541e50565067d49e6ed7aad1fad6984efc
Crustbrns <georgejoof@gmail.com>
2023-05-23 02:14:59 +0300

    Added sick features

--- Assets/PlayerCharacterisictics.cs
+++ Assets/PlayerCharacterisictics.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerCharacterisictics : MonoBehaviour
 {
     public GameObject canvas;
+    public GameObject damageIndicator;
     public float Health = 100;
     private bool isAlive = true;
 
@@ -13,6 +15,9 @@ public class PlayerCharacterisictics : MonoBehaviour
         if(this.isAlive)
         {
             this.GetComponentInParent<SoundManager>().Play(3, 1f);
+            var color = damageIndicator.GetComponent<Image>().color;
+            color.a = 0.8f;
+            damageIndicator.GetComponent<Image>().color = color;
             this.Health -= damage;
             if(this.Health <= 0)
             {
@@ -26,6 +31,13 @@ public class PlayerCharacterisictics : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.C))
         {
             TakeDamage(15);
+        }
+
+        if(damageIndicator.GetComponent<Image>().color.a > 0)
+        {
+            var color = damageIndicator.GetComponent<Image>().color;
+            color.a -= 0.5f * Time.deltaTime;
+            damageIndicator.GetComponent<Image>().color = color;
         }
     }
 
