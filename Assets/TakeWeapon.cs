be2441adcd02ca92ac9bba67719bbe5ca6403d35
Crustbrns <georgejoof@gmail.com>
2023-05-28 16:59:14 +0300

    Added handling gun system

---
+++ Assets/TakeWeapon.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TakeWeapon : MonoBehaviour
+{
+    public float TimeInteracted;
+    public float TimeToDoneInteraction;
+    public AudioSource source;
+    public AudioClip sound;
+
+    public bool Interacted = false;    
+    public Image Interaction;
+
+    public GameObject weaponInstance;
+    public GameObject parentInstance;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(!Interacted && enabled)
+        {
+            if(Input.GetKey(KeyCode.E))
+            {
+                TimeInteracted += Time.deltaTime;
+                if(TimeInteracted > TimeToDoneInteraction)
+                {
+                    this.tag = "Untagged";
+                    source.PlayOneShot(sound, 0.2f);
+                    Interacted = true;
+
+                    GetComponentInParent<Outline>().enabled = false;
+                    GetComponent<TitleHandler>().Scaling = false;
+                    weaponInstance.transform.localScale = new Vector3(0, 0, 0);
+                    // GetComponent<TakeWeapon>().enabled = false;
+                    Destroy(parentInstance, 1f);
+
+                    // GetComponent<DestroyInstance>().enabled = true;
+                }
+            }
+            else
+            {
+                if(TimeInteracted > 0)
+                {
+                    TimeInteracted -= Time.deltaTime * 3;
+                    if(TimeInteracted < 0)
+                    {
+                        TimeInteracted = 0;
+                    }
+                }
+            }
+
+            Interaction.fillAmount = TimeInteracted / TimeToDoneInteraction;
+        }
+    }
+}
