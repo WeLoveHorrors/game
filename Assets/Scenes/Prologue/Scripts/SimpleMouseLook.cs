92e90edf41171fc8639f92013ca1cd9f30a4fbe6
Crustbrns <georgejoof@gmail.com>
2023-06-07 22:26:17 +0300

    Added legs of marty

--- Assets/Scenes/Prologue/Scripts/SimpleMouseLook.cs
+++ Assets/Scenes/Prologue/Scripts/SimpleMouseLook.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class SimpleMouseLook : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float mouseSensitivity = 145f;
+
+    float RotationX = 0f;
+    float RotationY = 0f;
+
+
+     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        RotationY += Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        RotationX -= Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        RotationX = Mathf.Clamp(RotationX, -60f, 4f);
+        RotationY = Mathf.Clamp(RotationY, 0f, 180f);
+
+        transform.eulerAngles = new Vector3(RotationX, RotationY, 0);
     }
 }
