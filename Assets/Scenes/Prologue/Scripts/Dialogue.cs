6bd99033735b0d9515a7aaabdab3fbf73e469184
Crustbrns <georgejoof@gmail.com>
2023-06-09 00:27:54 +0300

    Added advanced dialogues' system

--- Assets/Scenes/Prologue/Scripts/Dialogue.cs
+++ Assets/Scenes/Prologue/Scripts/Dialogue.cs
@@ -7,10 +7,18 @@ public class Dialogue : MonoBehaviour
 {
     public string Question;
     public List<Tuple<string, string>> AnswearsAndReplies;
+    public bool hasAnswears = false;
 
+    public Dialogue(string Question)
+    {
+        this.Question = Question;
+        this.AnswearsAndReplies = new List<Tuple<string, string>>();
+        this.hasAnswears = false;
+    }
     public Dialogue(string Question, List<Tuple<string, string>> AnswearsAndReplies)
     {
         this.Question = Question;
         this.AnswearsAndReplies = AnswearsAndReplies;
+        this.hasAnswears = true;
     }
 }
--- Assets/Scenes/Prologue/Scripts/DialoguesManager.cs
+++ Assets/Scenes/Prologue/Scripts/DialoguesManager.cs
@@ -17,12 +17,13 @@ public class DialoguesManager : MonoBehaviour
     private float TextIndex = 0;
     private int CurrentDialogue = 0;
     private string TextGoal = "";
-    private bool isQuestion = false;
+    private bool needToReply = false;
     void Start()
     {
         Dialogues = new List<Dialogue>();
-        Dialogues.Add(new Dialogue("Test dialogue 1", new List<Tuple<string, string>>(){ new Tuple<string, string>("Answear 1", "asd"), new Tuple<string, string>("Answear 2", "qwe"), new Tuple<string, string>("Answear 3", "qwe") }));
-        Dialogues.Add(new Dialogue("Test dialogue 1", new List<Tuple<string, string>>(){ new Tuple<string, string>("Answear 1 qweqweasd", "asdzxcg s"), new Tuple<string, string>("Answear 2 dgdfgvbcbbt", "qweytryrt"), new Tuple<string, string>("Answear 3 aasqweetr", "qwejhg  hjg") }));
+        Dialogues.Add(new Dialogue("Марті.."));
+        Dialogues.Add(new Dialogue("Марті, ти мене чуєш?"));
+        Dialogues.Add(new Dialogue("В нас проблеми, Марті", new List<Tuple<string, string>>(){ new Tuple<string, string>("Що коїться, Док?", "Я ледве встиг тебе врятувати, як в нас пройшла сутичка з тими бандитами."), new Tuple<string, string>("Де ми?", "На нас напали, бо я вкрав пляшечку львівського. Тебе вирубили, я ледве встиг нас з тобою врятувати.."), new Tuple<string, string>("Куди ми летимо?", "В цьому і проблема.. Коли ми намагалися втікти, то пошкодили наш пристрій для подорожів у часі..") }));
         
         SetDialogues();
         AnswearsPanel.SetActive(false);
@@ -39,12 +40,14 @@ public class DialoguesManager : MonoBehaviour
             else
             {
                 isTextRendered = true;
-                Cursor.visible = true;
-                Cursor.lockState = CursorLockMode.None;
 
-                if(isQuestion)
+                if(needToReply && Dialogues[CurrentDialogue].hasAnswears)
                 {
                     Invoke("UnlockPanel", 1.5f);
+                }
+                else
+                {
+                    Invoke("RunNext", 2f);
                 }
             }
         }
@@ -52,11 +55,13 @@ public class DialoguesManager : MonoBehaviour
 
     private void SetDialogues()
     {
-        int AnswearIndex = 0;
         TextIndex = 0;
-        Answears.ForEach(x=> x.text = Dialogues[CurrentDialogue].AnswearsAndReplies[AnswearIndex++].Item1);
-        // Question.text = Dialogues[CurrentDialogue].Question;
-        isQuestion = true;
+        if(Dialogues[CurrentDialogue].hasAnswears)
+        {
+            int AnswearIndex = 0;
+            Answears.ForEach(x=> x.text = Dialogues[CurrentDialogue].AnswearsAndReplies[AnswearIndex++].Item1);
+            needToReply = true;
+        }
         Question.text = "";
         TextGoal = Dialogues[CurrentDialogue].Question;
     }
@@ -64,28 +69,34 @@ public class DialoguesManager : MonoBehaviour
     private void UnlockPanel()
     {
         AnswearsPanel.SetActive(true);
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
     }
 
     private void RunNext()
     {
-        TextIndex = 0;
-        CurrentDialogue++;
-        Cursor.visible = true;
-        Cursor.lockState = CursorLockMode.None;
-        isTextRendered = false;
-        isQuestion = true;
-        SetDialogues();
+        if(CurrentDialogue < Dialogues.Count)
+        {
+            TextIndex = 0;
+            CurrentDialogue++;
+
+            isTextRendered = false;
+            // needToReply = true;
+            SetDialogues();
+        }
     }
 
     public void AnswearToQuestion(int number)
     {
-        isQuestion = false;
+        needToReply = false;
         TextIndex = 0;
         AnswearsPanel.SetActive(false);
         isTextRendered = false;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
 
         TextGoal = Dialogues[CurrentDialogue].AnswearsAndReplies[number].Item2;
-        Invoke("RunNext", 4f);
+        // Invoke("RunNext", 4f);
         // Question.text = 
         // Question.text
     }
